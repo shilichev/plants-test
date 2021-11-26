aa93e837c20e521310a0fcb0c69b3fc06c36d1eb
ygug <yigig>
2021-11-26 18:14:29 +0300

    test

---
+++ height.js
@@ -0,0 +1,5 @@
+  let vh = window.innerHeight * 0.01;
+  let vw = window.innerWidth * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+  document.documentElement.style.setProperty("--vw", `${vw}px`);
+  console.log(vh);
