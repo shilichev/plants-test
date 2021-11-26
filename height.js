706761b1c785bf38f39f61675822ae5daab62336
ygug <yigig>
2021-11-26 18:39:05 +0300

    blur1

--- height.js
+++ height.js
@@ -2,4 +2,3 @@
   let vw = window.innerWidth * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
   document.documentElement.style.setProperty("--vw", `${vw}px`);
-  console.log(vh);
